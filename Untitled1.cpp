80f745e8c710dca174b407ae3e71a2ee8d998504
silvajimbyron-collab <silvajimbyron@gmail.com>
2025-09-20 15:25:39 +0800

    activity 1

---
+++ Untitled1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string name;
+    
+    cout << "Jim Byron Silva: ";
+    cin >> name;
+    cout << "Welcome to the CS 131 course, " << name << endl;
+    
+    return 0;
+    
+}
